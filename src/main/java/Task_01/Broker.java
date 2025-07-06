4097131dbca9f0bfaf8c9afa059c96e8b9f5c150
Pamali Weerasinghe <pamaliweerasinghe@gmail.com>
2025-07-06 16:18:33 +0530

    Created the Broker class

--- src/main/java/Task_01/Broker.java
+++ src/main/java/Task_01/Broker.java
@@ -1,11 +1,13 @@
 package Task_01;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Broker {
     //The port which the Broker will be listening for connections
@@ -19,5 +21,20 @@ public class Broker {
     //Each PrintWriter represents one connected subscriber
     
     private Map<String, List<PrintWriter>> topicSubscribers= new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        new Broker().start();
+    }
+
+    public void start(){
+        executorService= Executors.newCachedThreadPool();
+        try{
+            serverSocket=new ServerSocket(PORT);
+        }catch (IOException e){
+            System.err.println("Error shutting down server: "+e.getMessage());
+        }
+    }
+
+
 
 }
