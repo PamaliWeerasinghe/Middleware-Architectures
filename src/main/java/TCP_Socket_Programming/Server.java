8f20f1bca130f9a4e2e02e24522e077b7c3c0769
Pamali Weerasinghe <pamaliweerasinghe@gmail.com>
2025-08-01 09:22:13 +0530

    Altered TCP socket programming -> Sending data from Server to the Client

--- src/main/java/TCP_Socket_Programming/Server.java
+++ src/main/java/TCP_Socket_Programming/Server.java
@@ -7,9 +7,20 @@ public class Server {
     private static final Integer PORT = 5000;
     public static void main(String[] args) {
         try{
+            //check if the port number is passed
+//            if(args.length<1){
+//                System.out.println("Usage: java Server <Port>");
+//                return;
+//            }
+            //Read the port number from the CLI
+//            int port = Integer.parseInt(args[0]);
+            //Create a server socket with the given port
             ServerSocket serverSocket= new ServerSocket(PORT);
+
             System.out.println("Waiting for Client");
+
             Socket socket = serverSocket.accept();
+
             System.out.println("Client request accepted");
 
             //Sending data from the server to the client
